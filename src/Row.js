5e821efa966ddc01f77a986776be0b430b594f07
rkf2778 <vividrohit@gmail.com>
2020-10-25 22:12:00 +0530

    Banner and basic structure completed

--- src/Row.js
+++ src/Row.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
+import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -14,7 +15,7 @@ function Row({ title, fetchUrl }) {
     }
     fetchData();
   }, [fetchUrl]);
-  console.log(movies)
+  console.log(movies);
 
   return (
     <div className="row">
@@ -22,10 +23,13 @@ function Row({ title, fetchUrl }) {
 
       <div className="row__posters">
         {movies.map((movie) => (
-          <img 
-            src={`${base_url}${movie.poster_path}`} 
-            alt={movie.name} 
-            className="row__poster"
+          <img
+            key={movie.id}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
           />
         ))}
       </div>
